72be7bb4e964022b951e6882280051ac7f06bb0c
TRIDIBESH DEBNATH <112503254+Tridibesh-033@users.noreply.github.com>
2025-04-02 15:12:09 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "rabindra":"https://www.youtube.com/watch?v=J03Ts-HdzGI",
+    "hemanta":"https://www.youtube.com/watch?v=XpYWLmtvvII",
+    "skyfall":"https://www.youtube.com/watch?v=sZrTJesvJeo",
+    "demon":"https://www.youtube.com/watch?v=C41VZu__AWE&list=RDEMdkBfq6noCa92obd5KrYG0A&index=28"
+}
